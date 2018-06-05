a37dc444f1065c866bd9d9af7bcfa64b6820f34e
unknown <stephane.rodolausse.pf1@gmail.com>
2018-06-05 12:06:59 +0200

    comit du midi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Mairie de MONTREUIL';
+  user = {
+    name : 'Your Name here',
+    jobTitle : 'Your job here',
+    image : 'http://unena.rosselcdn.net/sites/default/files/dpistyles_v2/ena_16_9_extra_big/2017/05/07/node_29596/2659653/public/2017/05/07/B9711945478Z.1_20170507201155_000%2BGKH913948.1-0.jpg?itok=VlQr9JO3'
+  };
+  userSaisi = {
+    name : '',
+    jobTitle : '',
+    image : ''
+    };
+    color = 'black';
+  changeName(newName) {
+    console.log(newName);
+    this.userSaisi.name = newName.target.value;
+  }
+  changejobTitle(newJob) {
+    console.log(newJob);
+    this.userSaisi.jobTitle = newJob.target.value;
+  }
+  changePhotoUrl(newLink) {
+    console.log(newLink);
+    this.userSaisi.image = newLink.target.value;
+  }
+  saveValues(e) {
+    e.preventDefault();
+    this.user.name = this.userSaisi.name;
+    this.user.jobTitle = this.userSaisi.jobTitle;
+    this.user.image = this.userSaisi.image;
+  }
+
 }
